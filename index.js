0a8ab49f21ce2f89430f490101d956177e32242e
LAPTOP-EHDI9A5M\sarat <sarath@arminotech.in>
2022-08-13 12:01:47 +0530

    updated

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const { ApolloServer} = require("apollo-server")
+const { ApolloServer } = require("apollo-server")
 const resolvers = require("./graphQl/resolver")
 const typeDefs = require("./graphQl/schema")
+const mongoose = require("mongoose")
+
 
 const {
     ApolloServerPluginLandingPageLocalDefault
 } = require('apollo-server-core');
+const { db } = require("./config");
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     csrfPrevention: true,
-
     cache: 'bounded',
     // introspection: true,
     context: (req, res) => { return req },
@@ -18,7 +20,14 @@ const server = new ApolloServer({
         ApolloServerPluginLandingPageLocalDefault({ embed: true }),
     ],
 });
-
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+mongoose.connect(
+    db.url, {
+    // useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("data base is connected")
+    server.listen().then(({ url }) => {
+        console.log(`🚀  Server ready at ${url}`);
+    });
+})
